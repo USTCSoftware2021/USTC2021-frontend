da630ccefa71413c379cdf59bc01a62d592416be
psi-cmd <psi-cmd@outlook.com>
2021-09-29 15:00:50 +0800

    change to post

--- Profession/profession-index/main.js
+++ Profession/profession-index/main.js
@@ -1,6 +1,7 @@
 var barWord = document.getElementsByClassName("barword")
 var barWordA = document.getElementsByClassName("barworda")
 var inputBtn = document.getElementsByClassName("btn-border")[0]
+var input = document.getElementsByClassName("search")[0]
 
 for (let i = 0; i < 4; i++) {
     barWord[i].addEventListener("mouseover", () => {
@@ -25,11 +26,24 @@ for (let i = 0; i < 4; i++) {
 }
 
 inputBtn.addEventListener("click", () => {
-    fetch("/api").then(
-        (res) => {
-            console.log(res)
-        }
-    )
+    postData('/api', {sequence: input.value})
+        .then(data => console.log(data)) // JSON from `response.json()` call
+        .catch(error => console.error(error))
 })
+
+function postData(url, data) {
+  // Default options are marked with *
+  return fetch(url, {
+    body: JSON.stringify(data), // must match 'Content-Type' header
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // *client, no-referrer
+    headers: {
+        'content-type': 'application/json'
+      },
+  })
+  .then(response => response.json()) // parses response to JSON
+}
 
 
