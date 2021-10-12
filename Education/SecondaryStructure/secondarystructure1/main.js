63c6cbbcabc4a7eff93f00eb3d25e62d70d05be0
lhq <lhq541381890@mail.ustc.edu.cn>
2021-10-12 22:45:55 +0800

    done the secondary structure pages

---
+++ Education/SecondaryStructure/secondarystructure1/main.js
@@ -0,0 +1,28 @@
+var barWord = document.getElementsByClassName("barword")
+var barWordA = document.getElementsByClassName("barworda")
+
+
+for(let i = 0; i < 4; i++){
+    barWord[i].addEventListener("mouseover", () => {
+        barWord[i].style.background = "white"
+        barWord[i].style.cursor = "pointer"
+        barWordA[i].style.color = "black"
+
+    })
+    barWord[i].addEventListener("mouseout", () => {
+        // console.log("ok");
+        barWord[i].style.background = "black"
+        barWordA[i].style.color = "white"
+    })
+    barWordA[i].addEventListener("mouseover", () => {
+        barWord[i].style.background = "white"
+        barWordA[i].style.color = "black"
+    })
+    barWordA[i].addEventListener("mouseout", () => {
+        barWordA[i].style.color = "white"
+        barWord[i].style.background = "black"
+    })
+}
+
+
+    
